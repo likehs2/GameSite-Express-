bc31dcbd9c6c4bc9709ff05176b390ef216b816d
Vinicius Lopes <sfcvinicius@hotmail.com>
2023-05-29 10:54:40 -0300

    CriacaoPaginaContato
    Criação da pagina de contato, inicio da customização de envio de e-mail
    Criação do e-mail contato.spcegames@gmail.com

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const personRoutes = require('./routes/personRoutes')
 const mustacheExpress = require('mustache-express');
+const engine = mustacheExpress()
 const app = express()
+const path = require('path')
 
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.engine('.mustache', mustacheExpress());
-app.set('template engine', 'mustache');
+app.engine(".mustache", engine);
+app.set("views", path.join(__dirname, "templates"));
+app.set("view engine", "mustache")
 
 
 app.get('/', (req, res) =>{
-    res.render('index');
+    res.render("contato");
+})
+
+app.listen(3000, () =>{
+    console.log('Servidor iniciado...')
 })
