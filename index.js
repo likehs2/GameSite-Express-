29aaec31714754f35ce02c3353023bb56b65d444
Vinicius Lopes <sfcvinicius@hotmail.com>
2023-05-29 14:42:25 -0300

    EnvioDeEmail
    Envio de email pelo Form dentro do mustache

--- index.js
+++ index.js
@@ -15,8 +15,37 @@ app.set("view engine", "mustache")
 
 
 app.get('/', (req, res) =>{
+    res.render("login");
+})
+
+app.get('/conteudo', (req, res) =>{
+    res.render("sobreSite");
+})
+
+app.get('/tecnologia', (req, res) =>{
+    res.render("tecnologias");
+})
+
+app.get('/sobreCriador', (req, res) =>{
+    res.render("sobreCriador");
+})
+
+app.get('/contato', (req, res) =>{
     res.render("contato");
 })
+
+app.post('/tecnologia', (req, res) =>{
+    res.redirect('/tecnologias');
+})
+
+app.post('/sobreCriador', (req, res) =>{
+    res.redirect('/sobreCriador');
+})
+
+app.post('/contato', (req, res) =>{
+    res.redirect('/contato');
+})
+
 
 app.listen(3000, () =>{
     console.log('Servidor iniciado...')
